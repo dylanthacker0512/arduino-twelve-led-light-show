d5f8a83f45c756d2f4abd71c9dd7717607fd2f97
Dylan Jay Thacker <dylanthacker2009@icloud.com>
2025-08-20 09:54:54 +0100

    12 LED Light Show for Arduino - Code

---
+++ 12_LED_Light_Show/12_LED_Light_Show.ino
@@ -0,0 +1,236 @@
+/* 
+ */
+
+void setup() {
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(7, OUTPUT);
+  pinMode(8, OUTPUT);
+  pinMode(9, OUTPUT);
+  pinMode(10, OUTPUT);
+  pinMode(11, OUTPUT);
+  pinMode(12, OUTPUT);
+  pinMode(14, OUTPUT);
+}
+
+void loop() {
+  // TURNS ALL LEDS ON AT ONCE
+  digitalWrite(2, HIGH);
+  digitalWrite(3, HIGH);
+  digitalWrite(4, HIGH);
+  digitalWrite(5, HIGH);
+  digitalWrite(6, HIGH);
+  digitalWrite(7, HIGH);
+  digitalWrite(8, HIGH);
+  digitalWrite(9, HIGH);
+  digitalWrite(10, HIGH);
+  digitalWrite(11, HIGH);
+  digitalWrite(12, HIGH);
+  digitalWrite(14, HIGH);
+  delay(2000); // WAITS 2 SECONDS
+  // TURNS ALL LEDS OFF AT ONCE
+  digitalWrite(2, LOW);
+  digitalWrite(3, LOW);
+  digitalWrite(4, LOW);
+  digitalWrite(5, LOW);
+  digitalWrite(6, LOW);
+  digitalWrite(7, LOW);
+  digitalWrite(8, LOW);
+  digitalWrite(9, LOW);
+  digitalWrite(10, LOW);
+  digitalWrite(11, LOW);
+  digitalWrite(12, LOW);
+  digitalWrite(14, LOW);
+  delay(2000); // WAITS 2 SECONDS
+  // TURNS ALL LEDS ON AT ONCE
+  digitalWrite(2, HIGH);
+  digitalWrite(3, HIGH);
+  digitalWrite(4, HIGH);
+  digitalWrite(5, HIGH);
+  digitalWrite(6, HIGH);
+  digitalWrite(7, HIGH);
+  digitalWrite(8, HIGH);
+  digitalWrite(9, HIGH);
+  digitalWrite(10, HIGH);
+  digitalWrite(11, HIGH);
+  digitalWrite(12, HIGH);
+  digitalWrite(14, HIGH);
+  delay(2000); // WAITS 2 SECONDS
+  // TURNS ALL LEDS OFF AT ONCE
+  digitalWrite(2, LOW);
+  digitalWrite(3, LOW);
+  digitalWrite(4, LOW);
+  digitalWrite(5, LOW);
+  digitalWrite(6, LOW);
+  digitalWrite(7, LOW);
+  digitalWrite(8, LOW);
+  digitalWrite(9, LOW);
+  digitalWrite(10, LOW);
+  digitalWrite(11, LOW);
+  digitalWrite(12, LOW);
+  digitalWrite(14, LOW);
+  delay(2000); // WAITS 2 SECONDS
+  //TURNS ALL RED LEDS ON
+  digitalWrite(2, HIGH);
+  digitalWrite(5, HIGH);
+  digitalWrite(8, HIGH);
+  digitalWrite(11, HIGH);
+  delay(500); // WAITS 0.5 SECONDS
+  //TURNS ALL RED LEDS OFF
+  digitalWrite(2, LOW);
+  digitalWrite(5, LOW);
+  digitalWrite(8, LOW);
+  digitalWrite(11, LOW);
+  //TURNS ALL YELLOW LEDS ON
+  digitalWrite(3, HIGH);
+  digitalWrite(6, HIGH);
+  digitalWrite(9, HIGH);
+  digitalWrite(12, HIGH);
+  delay(500); // WAITS 0.5 SECONDS
+  //TURNS ALL YELLOW LEDS OFF
+  digitalWrite(3, LOW);
+  digitalWrite(6, LOW);
+  digitalWrite(9, LOW);
+  digitalWrite(12, LOW);
+  //TURNS ALL GREEN LEDS ON
+  digitalWrite(4, HIGH);
+  digitalWrite(7, HIGH);
+  digitalWrite(10, HIGH);
+  digitalWrite(14, HIGH);
+  delay(500); //WAITS 0.5 SECONDS
+  digitalWrite(4, LOW);
+  digitalWrite(7, LOW);
+  digitalWrite(10, LOW);
+  digitalWrite(14, LOW);
+  delay(500); //WAITS 0.5 SECONDS
+  // FLASH ALL LIGHTS BY TURNING LEDS ON AND OFF ONE BY ONE
+  digitalWrite(2, HIGH);
+  delay(100);
+  digitalWrite(3, HIGH);
+  digitalWrite(2, LOW);
+  delay(100);
+  digitalWrite(4, HIGH);
+  digitalWrite(3, LOW);
+  delay(100);
+  digitalWrite(5, HIGH);
+  digitalWrite(4, LOW);
+  delay(100);
+  digitalWrite(6, HIGH);
+  digitalWrite(5, LOW);
+  delay(100);
+  digitalWrite(7, HIGH);
+  digitalWrite(6, LOW);
+  delay(100);
+  digitalWrite(8, HIGH);
+  digitalWrite(7, LOW);
+  delay(100);
+  digitalWrite(9, HIGH);
+  digitalWrite(8, LOW);
+  delay(100);
+  digitalWrite(10, HIGH);
+  digitalWrite(9, LOW);
+  delay(100);
+  digitalWrite(11, HIGH);
+  digitalWrite(10, LOW);
+  delay(100);
+  digitalWrite(12, HIGH);
+  digitalWrite(11, LOW);
+  delay(100);
+  digitalWrite(14, HIGH);
+  digitalWrite(12, LOW);
+  delay(100);
+  digitalWrite(12, HIGH);
+  digitalWrite(14, LOW);
+  delay(100);
+  digitalWrite(11, HIGH);
+  digitalWrite(12, LOW);
+  delay(100);
+  digitalWrite(10, HIGH);
+  digitalWrite(11, LOW);
+  delay(100);
+  digitalWrite(9, HIGH);
+  digitalWrite(10, LOW);
+  delay(100);
+  digitalWrite(8, HIGH);
+  digitalWrite(9, LOW);
+  delay(100);
+  digitalWrite(7, HIGH);
+  digitalWrite(8, LOW);
+  delay(100);
+  digitalWrite(6, HIGH);
+  digitalWrite(7, LOW);
+  delay(100);
+  digitalWrite(5, HIGH);
+  digitalWrite(6, LOW);
+  delay(100);
+  digitalWrite(4, HIGH);
+  digitalWrite(5, LOW);
+  delay(100);
+  digitalWrite(3, HIGH);
+  digitalWrite(4, LOW);
+  delay(100);
+  digitalWrite(2, HIGH);
+  digitalWrite(3, LOW);
+  delay(100);
+  digitalWrite(2, LOW);
+  delay(2000);
+   // TURNS ALL LEDS ON AT ONCE
+  digitalWrite(2, HIGH);
+  digitalWrite(3, HIGH);
+  digitalWrite(4, HIGH);
+  digitalWrite(5, HIGH);
+  digitalWrite(6, HIGH);
+  digitalWrite(7, HIGH);
+  digitalWrite(8, HIGH);
+  digitalWrite(9, HIGH);
+  digitalWrite(10, HIGH);
+  digitalWrite(11, HIGH);
+  digitalWrite(12, HIGH);
+  digitalWrite(14, HIGH);
+  delay(2000); // WAITS 2 SECONDS
+  // TURNS ALL LEDS OFF AT ONCE
+  digitalWrite(2, LOW);
+  digitalWrite(3, LOW);
+  digitalWrite(4, LOW);
+  digitalWrite(5, LOW);
+  digitalWrite(6, LOW);
+  digitalWrite(7, LOW);
+  digitalWrite(8, LOW);
+  digitalWrite(9, LOW);
+  digitalWrite(10, LOW);
+  digitalWrite(11, LOW);
+  digitalWrite(12, LOW);
+  digitalWrite(14, LOW);
+  delay(2000); // WAITS 2 SECONDS
+  // TURNS ALL LEDS ON AT ONCE
+  digitalWrite(2, HIGH);
+  digitalWrite(3, HIGH);
+  digitalWrite(4, HIGH);
+  digitalWrite(5, HIGH);
+  digitalWrite(6, HIGH);
+  digitalWrite(7, HIGH);
+  digitalWrite(8, HIGH);
+  digitalWrite(9, HIGH);
+  digitalWrite(10, HIGH);
+  digitalWrite(11, HIGH);
+  digitalWrite(12, HIGH);
+  digitalWrite(14, HIGH);
+  delay(2000); // WAITS 2 SECONDS
+  // TURNS ALL LEDS OFF AT ONCE
+  digitalWrite(2, LOW);
+  digitalWrite(3, LOW);
+  digitalWrite(4, LOW);
+  digitalWrite(5, LOW);
+  digitalWrite(6, LOW);
+  digitalWrite(7, LOW);
+  digitalWrite(8, LOW);
+  digitalWrite(9, LOW);
+  digitalWrite(10, LOW);
+  digitalWrite(11, LOW);
+  digitalWrite(12, LOW);
+  digitalWrite(14, LOW);
+  delay(5000);
+}
